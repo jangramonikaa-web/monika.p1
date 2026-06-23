5a90bed355806dbb03534a372644a0f44661a69d
jangramonikaa-web <jangramonikaa@gmail.com>
2026-06-23 23:49:54 +0530

    string method

---
+++ indexp3.py
@@ -0,0 +1,30 @@
+name="monika"
+print(name)
+print (type(name))
+
+text="hello"
+print(text.upper())
+
+message="hello world"
+print(message.replace("world","monika"))
+
+text2= " python programming"
+print(text.lower())
+
+text3=" i like python"
+print(text.capitalize())
+
+message2="python jave c++"
+print(message2.split()) 
+
+text5="pyhton programming"
+print(text5.find("p"))
+
+text6="python programming"
+print(len(text6))
+
+
+
+
+
+
