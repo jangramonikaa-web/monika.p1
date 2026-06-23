c5d2e8c3c2e485d8e5d4c576274a9efd4f85b945
jangramonikaa-web <jangramonikaa@gmail.com>
2026-06-23 23:55:23 +0530

    define type of variable

---
+++ indexp4.py
@@ -0,0 +1,16 @@
+age=18
+print(type(age))
+marks=89.7
+print(type(marks))
+
+name="monika"
+print(type(name))
+ 
+is_student=True
+print(type(is_student))
+
+fruits=["apple","banana","cherry"]
+print(type(fruits))
+
+colors=("red","green","blue")
+print(type(colors))
